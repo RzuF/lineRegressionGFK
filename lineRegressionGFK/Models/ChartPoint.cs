b540463682abadf3abeae46071687d1fac5f8e26
RzuF <RzuF@DESKTOP-EEBITK1>
2017-06-16 15:15:20 +0200

    ChartPoint documentation added

--- lineRegressionGFK/Models/ChartPoint.cs
+++ lineRegressionGFK/Models/ChartPoint.cs
@@ -10,10 +10,18 @@ using lineRegressionGFK.Annotations;
 
 namespace lineRegressionGFK.Models
 {
+    /// <summary>
+    /// Model of single chart point
+    /// </summary>
     public class ChartPoint
     {
+        /// <summary>
+        /// Property hold information about X coordinate
+        /// </summary>
         public double X { get; set; }
-
+        /// <summary>
+        /// Property hold information about Y coordinate
+        /// </summary>
         public double Y { get; set; }
     }
 }
