68f18fd726cbceb9b3cc6c87bc74d677f0c17353
RzuF <RzuF@DESKTOP-EEBITK1>
2017-06-16 15:27:12 +0200

    PolynomialRegression documentation added

--- lineRegressionGFK/Models/PolynomialRegression.cs
+++ lineRegressionGFK/Models/PolynomialRegression.cs
@@ -12,7 +12,9 @@ namespace lineRegressionGFK.Models
     public class PolynomialRegression : INotifyPropertyChanged
     {
         private List<ChartPolynomialPart> _graphicRepresentation;
-
+        /// <summary>
+        /// Property holds list of ChartPolynomialParts which are graphical representation of regression
+        /// </summary>
         public List<ChartPolynomialPart> GraphicRepresentation
         {
             get { return _graphicRepresentation; }
@@ -23,7 +25,13 @@ namespace lineRegressionGFK.Models
             }
         }
 
+        /// <summary>
+        /// Property holds array of coefficient of current polynomial
+        /// </summary>
         public double[] Coefficients { get; set; } = new double[0];
+        /// <summary>
+        /// Property holds information about text to display in frame
+        /// </summary>
         public string InfoString
         {
             get
@@ -37,8 +45,13 @@ namespace lineRegressionGFK.Models
             }
         }
 
+        /// <summary>
+        /// Impementation of INotifyPropertyChanged interface, neccessary for proper Binding behaviour.
+        /// </summary>
         public event PropertyChangedEventHandler PropertyChanged;
-
+        /// <summary>
+        /// Impementation of INotifyPropertyChanged interface, neccessary for proper Binding behaviour.
+        /// </summary>
         [NotifyPropertyChangedInvocator]
         protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
         {
